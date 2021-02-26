1664e43d325d22e2f3405a36fdee8b1af6bedb57
Danimjs <77038782+Danimjs@users.noreply.github.com>
2021-02-26 21:44:05 +1100

    Delete App.js

--- src/Components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-import { render } from '@testing-library/react';
-
-function App() {
-
-  
-export default App;
